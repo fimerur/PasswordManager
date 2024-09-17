6e1a3ebb4164632bed10c2bc0c8e104480827961
fimeru <fimerur@gmail.com>
2024-09-17 20:28:14 +0300

    Fix tabs

--- PasswordManager/PasswordApp.cs
+++ PasswordManager/PasswordApp.cs
@@ -1,101 +1,97 @@
-using System;
-using System.Collections.Generic;
+namespace PasswordManager;
 
-namespace PasswordManager
+public class PasswordApp
 {
-    public class PasswordApp
+    private const string MasterPassword = "мультипасс";
+        
+    private Dictionary<string, string> passwordStorage = new Dictionary<string, string>();
+        
+    public bool VerifyMasterPassword(string inputPassword)
     {
-        private const string MasterPassword = "мультипасс";
+        return inputPassword == MasterPassword;
+    }
         
-        private Dictionary<string, string> passwordStorage = new Dictionary<string, string>();
-        
-        public bool VerifyMasterPassword(string inputPassword)
+    public void AddPassword(string login, string password)
+    {
+        if (passwordStorage.ContainsKey(login))
         {
-            return inputPassword == MasterPassword;
+            Console.WriteLine($"Пароль для сервиса {login} уже существует.");
         }
-        
-        public void AddPassword(string login, string password)
+        else
         {
+            passwordStorage.Add(login, password);
+            Console.WriteLine($"Пароль для сервиса {login} успешно добавлен.");
+        }
+    }
+        
+    public bool SearchPassword(string login)
+    {
+        if (passwordStorage.ContainsKey(login))
+        {
+            Console.WriteLine($"Пароль для {login}: {passwordStorage[login]}");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine($"Пароль для сервиса {login} не найден.");
+            return false;
+        }
+    }
+
+    public void ChangePassword(string login)
+    {
+        bool validLogin = false;
+
+        while (!validLogin)
+        {
+
             if (passwordStorage.ContainsKey(login))
             {
-                Console.WriteLine($"Пароль для сервиса {login} уже существует.");
-            }
-            else
-            {
-                passwordStorage.Add(login, password);
-                Console.WriteLine($"Пароль для сервиса {login} успешно добавлен.");
-            }
-        }
-        
-        public bool SearchPassword(string login)
-        {
-            if (passwordStorage.ContainsKey(login))
-            {
-                Console.WriteLine($"Пароль для {login}: {passwordStorage[login]}");
-                return true;
+                bool passwordChanged = false;
+                while (!passwordChanged)
+                {
+                    Console.Write("Введите новый пароль: ");
+                    string newPassword = Console.ReadLine()!;
+
+                    if (passwordStorage[login] == newPassword)
+                    {
+                        Console.WriteLine("Новый пароль совпадает с текущим.");
+                        Console.WriteLine("Введите другой пароль или введите 'выход' для возврата в главное меню: ");
+                        string userInput = Console.ReadLine()!;
+
+                        if (userInput! == "выход")
+                        {
+                            passwordChanged = true;
+                        }
+                        else
+                        {
+                            newPassword = userInput;
+                        }
+                    }
+                    else
+                    {
+                        passwordStorage[login] = newPassword;
+                        Console.WriteLine($"Пароль для сервиса {login} успешно изменен.");
+                        passwordChanged = true;
+                    }
+                }
+
+                validLogin = true;
             }
             else
             {
                 Console.WriteLine($"Пароль для сервиса {login} не найден.");
-                return false;
-            }
-        }
 
-        public void ChangePassword(string login)
-        {
-            bool validLogin = false;
+                Console.Write("Попробуйте снова или введите 'выход' для возврата в главное меню: ");
+                string userInput = Console.ReadLine()!;
 
-            while (!validLogin)
-            {
-
-                if (passwordStorage.ContainsKey(login))
+                if (userInput! == "выход")
                 {
-                    bool passwordChanged = false;
-                    while (!passwordChanged)
-                    {
-                        Console.Write("Введите новый пароль: ");
-                        string newPassword = Console.ReadLine()!;
-
-                        if (passwordStorage[login] == newPassword)
-                        {
-                            Console.WriteLine("Новый пароль совпадает с текущим.");
-                            Console.WriteLine("Введите другой пароль или введите 'выход' для возврата в главное меню: ");
-                            string userInput = Console.ReadLine()!;
-
-                            if (userInput! == "выход")
-                            {
-                                passwordChanged = true;
-                            }
-                            else
-                            {
-                                newPassword = userInput;
-                            }
-                        }
-                        else
-                        {
-                            passwordStorage[login] = newPassword;
-                            Console.WriteLine($"Пароль для сервиса {login} успешно изменен.");
-                            passwordChanged = true;
-                        }
-                    }
-
                     validLogin = true;
                 }
                 else
                 {
-                    Console.WriteLine($"Пароль для сервиса {login} не найден.");
-
-                    Console.Write("Попробуйте снова или введите 'выход' для возврата в главное меню: ");
-                    string userInput = Console.ReadLine()!;
-
-                    if (userInput! == "выход")
-                    {
-                        validLogin = true;
-                    }
-                    else
-                    {
-                        login = userInput;
-                    }
+                    login = userInput;
                 }
             }
         }
--- PasswordManager/Program.cs
+++ PasswordManager/Program.cs
@@ -1,94 +1,90 @@
-﻿using System;
-using PasswordManager;
+﻿namespace PasswordManager;
 
-namespace PasswordManager
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            PasswordApp manager = new PasswordApp();
-            int attemptCount = 0;
-            const int maxAttempts = 3;
+        PasswordApp manager = new PasswordApp();
+        int attemptCount = 0;
+        const int maxAttempts = 3;
             
-            while (attemptCount < maxAttempts)
-            {
-                Console.Write("Введите мастер пароль: ");
-                string masterPassword = Console.ReadLine()!;
-                
-                if (manager.VerifyMasterPassword(masterPassword!))
-                {
-                    Console.WriteLine("Добро пожаловать в менеджер паролей!");
-                    
-                    while (true)
-                    {
-                        Console.WriteLine("Выберите действие:");
-                        Console.WriteLine("1. Добавить новый пароль");
-                        Console.WriteLine("2. Поиск пароля");
-                        Console.WriteLine("3. Изменить пароль");
-                        Console.WriteLine("4. Выход");
-                        Console.Write("Ваш выбор: ");
-                        string choice = Console.ReadLine()!;
+        while (attemptCount < maxAttempts)
+        {
+            Console.Write("Введите мастер пароль: ");
+            string masterPassword = Console.ReadLine()!;
 
-                        switch (choice)
-                        {
-                            case "1":
-                                Console.Write("Введите название сервиса: ");
-                                string login = Console.ReadLine()!;
-                                Console.Write("Введите пароль: ");
-                                string password = Console.ReadLine()!;
-                                manager.AddPassword(login, password);
-                                break;
-                            
-                            case "2":
-                                bool validLogin = true;
-                                while (validLogin)
-                                {
-                                    Console.Write("Введите название сервиса (или введите 'выход' для возврата в главное меню): ");
-                                    string searchlogin = Console.ReadLine()!;
-        
-                                    if (searchlogin! == "выход")
-                                    {
-                                        validLogin = true;
-                                    }
-                                    else
-                                    {
-                                        if (manager.SearchPassword(searchlogin))
-                                        {
-                                            validLogin = false;
-                                        }
-                                        else
-                                        {
-                                            Console.WriteLine("Пароль не найден. Попробуйте снова.");
-                                        }
-                                    }
-                                }
-                                break;
-                            
-                            case "3":
-                                Console.Write("Введите название сервиса: ");
-                                string loginToChange = Console.ReadLine()!;
-                                manager.ChangePassword(loginToChange); 
-                                break;
-                            
-                            case "4":
-                                Console.WriteLine("До свидания!");
-                                return;
-                            
-                            default:
-                                Console.WriteLine("Неверный формат.");
-                                break;
-                        }
-                    }
-                }
-                else
+            if (!manager.VerifyMasterPassword(masterPassword!))
+            {
+                attemptCount++;
+                Console.WriteLine($"Неверный мастер-пароль. Попытка {attemptCount} из {maxAttempts}.");
+
+                if (attemptCount != maxAttempts) continue;
+                Console.WriteLine("Превышено количество попыток ввода. Доступ заблокирован.");
+                return;
+            }
+
+            Console.WriteLine("Добро пожаловать в менеджер паролей!");
+
+            while (true)
+            {
+                Console.WriteLine("Выберите действие:");
+                Console.WriteLine("1. Добавить новый пароль");
+                Console.WriteLine("2. Поиск пароля");
+                Console.WriteLine("3. Изменить пароль");
+                Console.WriteLine("4. Выход");
+                Console.Write("Ваш выбор: ");
+                string choice = Console.ReadLine()!;
+
+                switch (choice)
                 {
-                    attemptCount++;
-                    Console.WriteLine($"Неверный мастер-пароль. Попытка {attemptCount} из {maxAttempts}.");
-                    
-                    if (attemptCount != maxAttempts) continue;
-                    Console.WriteLine("Превышено количество попыток ввода. Доступ заблокирован.");
-                    return;
+                    case "1":
+                        Console.Write("Введите название сервиса: ");
+                        string login = Console.ReadLine()!;
+                        Console.Write("Введите пароль: ");
+                        string password = Console.ReadLine()!;
+                        manager.AddPassword(login, password);
+                        break;
+
+                    case "2":
+                        bool validLogin = true;
+                        while (validLogin)
+                        {
+                            Console.Write(
+                                "Введите название сервиса (или введите 'выход' для возврата в главное меню): ");
+                            string searchlogin = Console.ReadLine()!;
+
+                            if (searchlogin! == "выход")
+                            {
+                                validLogin = true;
+                            }
+                            else
+                            {
+                                if (manager.SearchPassword(searchlogin))
+                                {
+                                    validLogin = false;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Пароль не найден. Попробуйте снова.");
+                                }
+                            }
+                        }
+
+                        break;
+
+                    case "3":
+                        Console.Write("Введите название сервиса: ");
+                        string loginToChange = Console.ReadLine()!;
+                        manager.ChangePassword(loginToChange);
+                        break;
+
+                    case "4":
+                        Console.WriteLine("До свидания!");
+                        return;
+
+                    default:
+                        Console.WriteLine("Неверный формат.");
+                        break;
                 }
             }
         }
